98253c760d2b18e49186fcfc8ebd35643a1f8d21
Sahil Wadhwa <sidashsahil@gmail.com>
2015-06-24 00:51:31 +0530

    Update script.php

--- script.php
+++ script.php
@@ -1,8 +1,8 @@
 <?php
 	$ch = curl_init();
-	$query = "dancers";
+	$query = "dancers in delhi";
 	$query = str_replace( " ", "+", $query);
-	$url = "https://www.googleapis.com/customsearch/v1?cx=010131248096801750602%3A0_r6wt_1kxy&cr=india&q==related%3A".$query."&sort=date&key=AIzaSyCDuAdM4YDWTAqdvrqZX65mufRaBayKAq4";
+	$url = "https://www.googleapis.com/customsearch/v1?cx=010131248096801750602%3A0_r6wt_1kxy&cr=india&q=allinlinks%3A+".$query."&sort=date&client=google-csbe&key=AIzaSyCDuAdM4YDWTAqdvrqZX65mufRaBayKAq4";
 	curl_setopt($ch, CURLOPT_URL, $url);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 	curl_setopt($ch, CURLOPT_HEADER, false);
@@ -12,6 +12,6 @@
 	
 	// converting the result in a php array
 	$s_results=json_decode($result);
-	for $i in $s_results:
-		secho $i->{'url'};
+	
+	var_dump($s_results);
 ?>
