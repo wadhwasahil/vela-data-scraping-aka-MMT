c719d9ba36d1a3b9653ab803b1cd8429548e7af2
wadhwasahil <sidashsahil@gmail.com>
2015-07-03 14:49:24 +0530

    Initial Commit

---
+++ sahil.php
@@ -0,0 +1,203 @@
+<?php include"header.html";?>
+<div class="container">
+  <div class="row">
+        <h2><strong>Match[my]Talent Search</strong></h2>     
+        <hr> 
+        <div class="dropdown">
+            <a id="dLabel" role="button" data-toggle="dropdown" class="btn btn-primary" data-target="#" href="/sahil.php">
+                Talent<span class="caret"></span>
+            </a>
+        <ul class="dropdown-menu multi-level" role="menu" aria-labelledby="dropdownMenu">
+              <li class="dropdown-submenu">
+                <a tabindex="-1" >Actor</a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Character Actor</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Child Actor</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Comic Actor</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Cross Gender Actor</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Specific Actors</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Stunt Actor</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Supporting Actor</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Theatre Actor</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">TV Actor</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Child Anchor</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Thriller Role</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Romantic Role</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Mythological Actor</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Lead Role</a></li>
+                  <li class="divider"></li>
+                </ul>
+              </li>
+              <li class="dropdown-submenu">
+                <a tabindex="-1">Dancer</a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Bollywood Classical</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Contemporary</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Folk</a></li>
+                  <li class="divider"></li>
+                </ul>
+              </li>
+              <li class="dropdown-submenu">
+                <a tabindex="-1" >Event Artists</a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">DJ</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Magician</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Mimic</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Puppeteer</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">RJ</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Standup Comedian</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">VJ</a></li>
+                  <li class="divider"></li>
+                </ul>
+              </li>
+              <li class="dropdown-submenu">
+                <a tabindex="-1">Model</a> 
+                <ul class="dropdown-menu">
+                  <li><a href="#">Alternative Model</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Art Model</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Commercial print and on-camera Model</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Fitness Model</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Glamour Model</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Parts Model</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Plus-size Model</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Promotional Model</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Runway Model</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Plus-size Model</a></li>
+                  <li class="divider"></li>
+                </ul>
+              </li>
+              <li class="dropdown-submenu">
+                <a tabindex="-1">Musician</a> 
+                <ul class="dropdown-menu">
+                  <li><a href="#">Band</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Brass Instrumentalist</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Clarinet Instrumentalist</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Classical Percussionist</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Drummer</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Electronic Instrumentalist</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Flute Player</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Guitarist</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Pianist</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">String Instrumentalist</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Wind Instrument Player</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Western Instrumentalist</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Xylophone Player</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Octapad</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Harmonium</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Duo</a></li>
+                  <li class="divider"></li>
+                </ul>
+              </li>
+              <li class="dropdown-submenu">
+                <a tabindex="-1">Singer</a> 
+                <ul class="dropdown-menu">
+                  <li><a href="#">Contemporary Singer</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Classical Singer</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Folk Singer</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Fusion Singer</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Jazz Singer</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">RAP</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Devotional</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Beatboxing</a></li>
+                  <li class="divider"></li>
+                </ul>
+              </li>
+              <li class="dropdown-submenu">
+                <a tabindex="-1">Stunts Artist</a> 
+                <ul class="dropdown-menu">
+                  <li><a href="#">Acrobats</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Aerialist</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Fire Performance</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Human Pyramid</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Juggler</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Knife Throwing</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Stilts</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Tight Rope Walking</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Trapeze</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Wheel gymnastics</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Stunt Rider</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Skateboard</a></li>
+                  <li class="divider"></li>
+                </ul>
+              </li>
+              <li class="dropdown-submenu">
+                <a tabindex="-1">Voice Artist</a> 
+                <ul class="dropdown-menu">
+                  <li><a href="#">Crew</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Character</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Mimic</a></li>
+                  <li class="divider"></li>
+                  <li><a href="#">Narrative</a></li>
+                  <li class="divider"></li>
+                </ul>
+              </li>
+            </ul>
+        </div>
+  </div>
+</div>
+
