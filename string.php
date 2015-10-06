776be8995e7dd3c1cc4c4b07c644953ec35e6548
wadhwasahil <sidashsahil@gmail.com>
2015-10-06 13:20:27 +0530

    Singular Talent Fixed

--- string.php
+++ string.php
@@ -9,7 +9,7 @@ include('connections.php');
 	$string2=""; 
 	$string3=""; 
 	 while($row = mysql_fetch_array($ret_val,MYSQL_ASSOC)){
-      	$string1=$row['name'];  
+      	$string1=$row['name'];
   	}
 	$query = 'select * from talents_specializations where id='.$special_id;
 	$ret_val = mysql_query($query,$conn);
@@ -24,5 +24,11 @@ include('connections.php');
   	$temp=$string3." ".$string2." ".$string1;
 	$result_text = preg_replace('/\b(\w+)\s+\\1\b/i', '$1', $temp);
 	$result_text=preg_replace("/[^A-Za-z0-9 ]/", ' ', $result_text);
-	echo $result_text.'s';
+	$result_text=$result_text.'s';
+	$result_text=trim($result_text," ");
+	$arr=explode(' ', $result_text);
+	if(count($arr)==1){
+		$result_text='"'.$result_text.'"';
+	}
+	echo $result_text;
 ?>
