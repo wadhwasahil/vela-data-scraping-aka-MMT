9f2f4983e78dcd05e00d249cb365c68656cfe716
wadhwasahil <sidashsahil@gmail.com>
2015-10-09 15:01:26 +0530

    More Bugs Fixed

--- string.php
+++ string.php
@@ -21,12 +21,32 @@ include('connections.php');
 	 while($row = mysql_fetch_array($ret_val,MYSQL_ASSOC)){
       	$string3=$row['name'];  
   	}
-  	$arr=explode(' ', $string2);
-  	if(strcmp($arr[0],'Specific')!=0)
+  	$a=explode(' ', $string3);
+  	$count=count($a);
+  	if(strcmp($a[0], 'Others')==0 || strcmp($a[0], 'Other')==0)
+  	{
+  		$string3="";
+  		for($i=1;$i<$count;$i++){
+			$string3.=$a[$i];
+			if($i!=$count-1)
+				$string3.=" ";
+		}
+  	}
+
+
+  	$a=explode(' ', $string2);
+  	$count=count($a);
+  	if(strcmp($a[0], 'Others')==0 || strcmp($a[0], 'Other')==0 || strcmp($a[0], 'Specific')==0)
+  	{
+  		$string2="";
+  		for($i=1;$i<$count;$i++){
+			$string2.=$a[$i];
+			if($i!=$count-1)
+				$string2.=" ";
+		}
+  	}
   	$temp=$string3." ".$string2." ".$string1;
-  	else
-  		$temp=$string3 ." ".$string1;
-	$result_text = preg_replace('/\b(\w+)\s+\\1\b/i', '$1', $temp);
+  	$result_text = preg_replace('/\b(\w+)\s+\\1\b/i', '$1', $temp);
 	$result_text=preg_replace("/[^A-Za-z0-9 ]/", ' ', $result_text);
 	$result_text=$result_text;
 	$result_text=trim($result_text," ");
