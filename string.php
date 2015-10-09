0c7b6406f754ae11f2275793fa51e23ea29c5c2e
wadhwasahil <sidashsahil@gmail.com>
2015-10-09 14:44:08 +0530

    More Bugs Fixed

--- string.php
+++ string.php
@@ -21,14 +21,24 @@ include('connections.php');
 	 while($row = mysql_fetch_array($ret_val,MYSQL_ASSOC)){
       	$string3=$row['name'];  
   	}
+  	$arr=explode(' ', $string2);
+  	if(strcmp($arr[0],'Specific')!=0)
   	$temp=$string3." ".$string2." ".$string1;
+  	else
+  		$temp=$string3 ." ".$string1;
 	$result_text = preg_replace('/\b(\w+)\s+\\1\b/i', '$1', $temp);
 	$result_text=preg_replace("/[^A-Za-z0-9 ]/", ' ', $result_text);
-	$result_text=$result_text.'s';
+	$result_text=$result_text;
 	$result_text=trim($result_text," ");
 	$arr=explode(' ', $result_text);
-	if(count($arr)==1){
+	$count=count($arr);
+	$arr[count($arr)-1]='"'.$arr[count($arr)-1].'"';
+	/*if(count($arr)==1){
 		$result_text='"'.$result_text.'"';
+	}*/
+	for($i=0;$i<$count;$i++){
+		echo $arr[$i];
+		if($i!=$count-1) echo " ";	
 	}
-	echo $result_text;
+	//echo $result_text;
 ?>
